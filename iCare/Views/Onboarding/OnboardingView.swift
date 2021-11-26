c7c801dcd8c0331483b0fbfcb62f8bc416db3427
jperez1478 <jessicperez3@gmail.com>
2021-11-26 15:28:55 -0600

    creating launch

---
+++ iCare/Views/Onboarding/OnboardingView.swift
@@ -0,0 +1,62 @@
+//
+//  OnboardingView.swift
+//  iCare
+//
+//  Created by Jessica Perez on 11/26/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    //MARK: -Properties
+////creates onboarding propertie
+
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    
+    //MARK: -Body
+    var body: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                //MARK: -Header
+                Spacer()
+                VStack(spacing: 20) {
+                    Text("""
+                        
+                        iCare
+                        Maternal & Prenatal Health
+                        
+                        """)
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                    Text("""
+                        Providing essential path
+                        for a more healthier pregnacy
+                        
+                        
+                        """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal,10)
+                    
+                }
+                
+                //MARK: -Header
+                
+                //MARK: -Footer
+            }
+        }//VStack
+    }//: ZStack
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
