98455f1e63d5be0dd245a398f65c57b47b632568
jperez1478 <jessicperez3@gmail.com>
2021-11-26 12:42:57 -0600

    Initial Commit

---
+++ iCare/iCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCareApp.swift
+//  iCare
+//
+//  Created by Jessica Perez on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct iCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
